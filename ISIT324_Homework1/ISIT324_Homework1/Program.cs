7951516bf96588bcc10996069f653a6cb903e582
Marcus Lazaro <55485519+lazarom37@users.noreply.github.com>
2023-01-12 16:24:13 -0800

    initialization

---
+++ ISIT324_Homework1/ISIT324_Homework1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ISIT324_Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /**
+            * Find last index of element
+            *
+            * @param x array to search
+            * @param y value to look for
+            * @return last index of y in x; -1 if absent
+            * @throw NullPointerException if x is null
+*/
+            int findLast(int[] x, int y)
+            {
+                for (int i = x.Length-1; i >= 0; i--) //FIXED: changed operator from = to >=
+                {
+                    if (x[i] == y)
+                    {
+                        return i;
+                    }
+                    Console.WriteLine(x[i] + " checked");
+                }
+                return -1;
+            }
+            // test: x = [2, 3, 5]; y = 2; Expected = -1
+
+            int[] x = {2, 3, 5};
+            int y = 0;
+            Console.Write("END RESULT: " + findLast(x, y));
+
+        }
+    }
+}
---
+++ ISIT324_Union/ISIT324_Union/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace ISIT324_Union
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList sample1 = new ArrayList() { 1, 2, 3 };
+            ArrayList sample2 = new ArrayList() { 4, 5, 6 };
+            foreach (var item in sample1)
+            {
+                Console.WriteLine("sample1: " + item);
+            }
+            Console.WriteLine();
+            foreach (var item in sample2)
+            {
+                Console.WriteLine("sample2: " + item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Here they are together: ");
+
+            foreach (var item in Union(sample1, sample2))
+            {
+                Console.WriteLine("final: " + item);
+            }
+        }
+
+        public static ArrayList Union(ArrayList a, ArrayList b)
+        {
+            //return a list of objects from two arrayLists
+            ArrayList final = new ArrayList();
+            foreach (var item in a)
+            {
+                final.Add(item);
+            }
+            foreach (var item in b)
+            {
+                final.Add(item);
+            }
+            return final;
+        }
+    }
+}
